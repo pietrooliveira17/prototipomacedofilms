7bf2c653598aa729fb269639c83a308994c42656
Pietro Sousa de Oliveira <75648885+pietrooliveira17@users.noreply.github.com>
2025-12-22 23:24:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const modal = document.getElementById('videoModal');
-    const container = document.getElementById('vimeo-player-container');
-    const triggers = document.querySelectorAll('.vimeo-trigger');
-    const closeBtn = document.querySelector('.close-modal');
+        const modal = document.getElementById('videoModal');
+        const container = document.getElementById('vimeo-player-container');
+        const triggers = document.querySelectorAll('.vimeo-trigger');
+        const closeBtn = document.querySelector('.close-modal');
 
-    triggers.forEach(trigger => {
-        trigger.addEventListener('click', function () {
-            const vimeoId = this.getAttribute('data-vimeo-id');
-            container.innerHTML = `
-                        <iframe src="https://player.vimeo.com/video/${vimeoId}?autoplay=1&color=556B2F" 
-                                style="position:absolute;top:0;left:0;width:100%;height:100%;" 
-                                frameborder="0" allow="autoplay; fullscreen" allowfullscreen>
-                        </iframe>`;
-            modal.style.display = 'flex';
-            document.body.style.overflow = 'hidden';
+        triggers.forEach(trigger => {
+            const openVideo = function() {
+                const vimeoId = this.getAttribute('data-vimeo-id');
+                container.innerHTML = `
+                    <iframe src="https://player.vimeo.com/video/${vimeoId}?autoplay=1&color=556B2F&dnt=1" 
+                            style="position:absolute;top:0;left:0;width:100%;height:100%;" 
+                            frameborder="0" allow="autoplay; fullscreen" allowfullscreen>
+                    </iframe>`;
+                modal.style.display = 'flex';
+                document.body.style.overflow = 'hidden';
+            };
+
+            trigger.addEventListener('click', openVideo);
+        });
+
+        const closeModal = (e) => {
+            if (e) e.preventDefault();
+            modal.style.display = 'none';
+            container.innerHTML = ''; 
+            document.body.style.overflow = 'auto';
+        };
+
+        closeBtn.addEventListener('click', closeModal);
+        closeBtn.addEventListener('touchstart', closeModal);
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) closeModal();
+        });
+        modal.addEventListener('touchstart', (e) => {
+            if (e.target === modal) closeModal();
         });
     });
-
-    const closeModal = () => {
-        modal.style.display = 'none';
-        container.innerHTML = '';
-        document.body.style.overflow = 'auto';
-    };
-
-    closeBtn.addEventListener('click', closeModal);
-    window.addEventListener('click', (e) => { if (e.target == modal) closeModal(); });
-});
 
 function checkCookies() {
     if (!localStorage.getItem('cookiesAccepted')) {
